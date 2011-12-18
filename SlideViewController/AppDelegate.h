e3d9b6593e21b57b4dbb13f9d5f1bb2afe9c74a7
Andrew Carter <andrew.r.carter@gmail.com>
2011-12-18 16:18:23 -0500

    Initial Commit

---
+++ SlideViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SlideViewController
+//
+//  Created by Andrew Carter on 12/18/11.
+//  Copyright (c) 2011 WillowTree Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
